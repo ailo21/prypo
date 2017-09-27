256d388191cef410780175a791fd737b047e177b
ailo21 <alex92ailo@mail.ru>
2017-09-27 21:05:28 +0300

    test1

---
+++ pg.dbase/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg.dbase
+{
+    public class Class1
+    {
+    }
+}
